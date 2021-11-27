2aa3bf42972439ed292b568a1c8285b90330b052
dahye1013 <alrean@naver.com>
2021-11-27 22:29:24 +0900

    feat : mongoDB 연결

--- server.js
+++ server.js
@@ -4,6 +4,20 @@
 const express = require("express");
 const app = express();
 
-app.listen(8080, function () {
-  console.log(`listening on 8080`);
+/**
+ * [Database]
+ * - mogoDB
+ */
+const MongoClient = require("mongodb").MongoClient;
+var db;
+const url = require("./store");
+
+MongoClient.connect(url.mongoUrl, function (error, client) {
+  if (error) return console.log(error);
+
+  db = client.db("todoapp"); // database명 할당;
+
+  app.listen(8080, function () {
+    console.log(`listening on 8080`);
+  });
 });
