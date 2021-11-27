a6fc2294c04665f85a5450e4afdd29ddaf74da0d
dahye1013 <alrean@naver.com>
2021-11-28 01:08:04 +0900

    feat : todo 입력시 id 할당

--- server.js
+++ server.js
@@ -41,11 +41,34 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/add", function (req, res) {
+app.post("/add", async function (req, res) {
   const { title, date } = req.body;
-  db.collection("post").insertOne({ title, date }, function (err, ok) {
-    console.log("success adding");
-  });
+  let totPostsCount = await db
+    .collection("counter")
+    .findOne({ name: "totalPosts" }, function (err, result) {
+      let totPostsCount = result.totalPostCount;
+      console.log(result.totalPostCount);
+
+      db.collection("post").insertOne(
+        {
+          _id: totPostsCount++,
+          title,
+          date,
+        },
+        function (err, ok) {
+          console.log("success adding");
+          db.collection("counter").updateOne(
+            { name: "totalPosts" },
+            //증감 연산자
+            { $inc: { totalPostCount: 1 } },
+            { totalPostCount: 1 },
+            function (err, result) {
+              if (err) return console.log(err);
+            }
+          );
+        }
+      );
+    });
   res.send("TODO 전송 완료");
 });
 
